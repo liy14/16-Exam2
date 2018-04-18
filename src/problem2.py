9cc1e96a490bfa97cca81da1734b448a5e78d253
liy14 <liy14@rose-hulman.edu>
2018-04-17 21:59:51 -0400

    FINISH

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Yi Li.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -154,9 +154,17 @@ def problem2(n, seq):
       :type seq:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new = []
+    for k in range(len(seq)):
+        if seq[k] < n:
+            new = new + [seq[k]]
+    if len(new) >= 3:
+        return [new[0], new[1], new[2]]
+    else:
+        return 'Too few'
 
 
 ###############################################################################
